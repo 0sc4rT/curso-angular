1ef96638b576dfb67787c42d5b500fa2290d92d0
mauricio.torrez@gmail.com <mauricio.torrez@gmail.com>
2016-10-12 22:25:34 -0400

    commit dia 9

---
+++ client/app/main/list/angular/angular.controller.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('uiApp')
+  .controller('AngularCtrl', function (NgTableParams) {
+    var vm = this;
+    var data = [
+      {
+        name: "Juan", 
+        age: 25
+      },
+      {
+        name: "Maria",
+        age: 26
+      }];
+    vm.tableParams = new NgTableParams({}, {dataset: data});
+  });
